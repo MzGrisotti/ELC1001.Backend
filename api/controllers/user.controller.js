3f45e488adbc712799dafa9bfa2c9d66f6c72e98
mzrisotti <mzgrisotti@inf.ufsm.br>
2020-04-18 20:30:59 -0300

    Created User.create method

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,9 +6,19 @@ exports.create = async (req, res) => {
     try {
         // Essa rota deve criar um novo usuário no banco de dados e criar um token
         // para ele (para gerar o token use o método definido no arquivo user.model.js).
-        
+        const user = new User(req.body);
+
+        const token = await user.generateAuthToken();
+        const dados = user.toJSON();
+
         // A resposta deve estar no formato { user: [dados do usuário], token: [token gerado]}
-        // Pesquise qual deve ser o código de retorno HTTP quando um novo recurso foi criado no banco.
+        // Pesquise qual deve ser o código de retorno HTTP quando um novo recurso foi criado no banco. 201
+
+        res.status(201).send({
+          user: dados,
+          token: token
+        })
+
     }
     catch(err) {
         console.error(err, err.message, err.stack);
@@ -26,7 +36,7 @@ exports.login = async (req, res) => {
         // JWT para o usuário.
 
         // A resposta deve estar no formato { user: [dados do usuário], token: [token gerado]}
-        // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
+        // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida. 202
     } catch (err) {
         console.error(err, err.message, err.stack);
 
@@ -39,10 +49,11 @@ exports.login = async (req, res) => {
 exports.getInfo = (req, res) => {
     try {
         // Essa rota deve retornar as informações do usuário que está fazendo a requisição.
-        
+
         // Você pode escolher como retornar os dados, contanto que todas as informações do usuário
         // (exceto informações como senha e tokens).
-        // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
+        // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida. 202
+
     } catch(err) {
         console.error(err, err.message, err.stack);
 
